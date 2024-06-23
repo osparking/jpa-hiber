ff135b8d35c0c6bad8a83d83ddf9d61defc760a3
osparking <jbpark03@gmail.com>
2024-06-24 06:21:10 +0900

    jpa 및hibernate - baeldung

---
+++ src/main/java/space/bum/jpa_hiber/JpaHiberApplication.java
@@ -0,0 +1,13 @@
+package space.bum.jpa_hiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHiberApplication.class, args);
+	}
+
+}
