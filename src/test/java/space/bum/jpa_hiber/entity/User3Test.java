3e0868b329e5915f8188874d43c521c66f1447e2
osparking <jbpark03@gmail.com>
2024-06-25 18:28:15 +0900

    개체 선언 기본값은 DB 필드 기본값보다 미약

--- src/test/java/space/bum/jpa_hiber/entity/User3Test.java
+++ src/test/java/space/bum/jpa_hiber/entity/User3Test.java
@@ -2,6 +2,7 @@ package space.bum.jpa_hiber.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,19 @@ class User3Test {
     assertEquals(user.getFirstName(), "길동");
     assertEquals(user.getAge(), 25);
     assertFalse(user.getLocked());
+  }
+
+  @Test
+  void saveUser_shouldSaveWithNullName() {
+    User3 user = new User3();
+    user.setFirstName(null);
+    user.setAge(null);
+    user.setLocked(null);
+    user = userRepository.save(user);
+
+    assertNull(user.getFirstName());
+    assertNull(user.getAge());
+    assertNull(user.getLocked());
   }
 
 }
