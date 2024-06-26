ae521eaacdb57e4bec71d68ebe4cafbfd5941ea9
osparking <jbpark03@gmail.com>
2024-06-26 14:29:21 +0900

    초기화된 public static final 멤버 사용 테이블명지정

--- src/main/java/space/bum/jpa_hiber/entity/Article.java
+++ src/main/java/space/bum/jpa_hiber/entity/Article.java
@@ -5,10 +5,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "ARTICLES")
+@Table(name = Article.TABLE_NAME)
 public class Article {
   @Id
   private Integer id;
+  public static final String TABLE_NAME = "ARTICLES";
   private String author;
   private String periodicalTitle;
 
