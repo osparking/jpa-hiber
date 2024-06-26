b0043f84ae7f6c5b1ecf494ef1e1e686690390c0
osparking <jbpark03@gmail.com>
2024-06-26 09:44:42 +0900

    기본 애노' 생략

--- src/main/java/space/bum/jpa_hiber/entity/Course.java
+++ src/main/java/space/bum/jpa_hiber/entity/Course.java
@@ -1,16 +1,13 @@
 package space.bum.jpa_hiber.entity;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
 public class Course {
 
-  @Basic
   @Id
   private int id;
 
-  @Basic
   private String name;
 }
