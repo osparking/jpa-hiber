0cdc02a6630cc54f8f7d9be2f5638eeee00753c9
osparking <jbpark03@gmail.com>
2024-06-27 14:42:20 +0900

    누락된 Id 멤버 지정

--- src/main/java/space/bum/jpa_hiber/entity/Company.java
+++ src/main/java/space/bum/jpa_hiber/entity/Company.java
@@ -5,11 +5,15 @@ import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Company {
+	@Id
+	@GeneratedValue
 	private Integer id;
 
 	private String name;
