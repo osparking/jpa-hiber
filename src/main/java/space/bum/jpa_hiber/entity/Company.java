2ad54c3b404c9017c94217d2d14883d502968eb6
osparking <jbpark03@gmail.com>
2024-06-27 14:22:27 +0900

    attributeoveride 사용한 열 이름 중복 해소

--- src/main/java/space/bum/jpa_hiber/entity/Company.java
+++ src/main/java/space/bum/jpa_hiber/entity/Company.java
@@ -1,19 +1,27 @@
 package space.bum.jpa_hiber.entity;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
 @Entity
 public class Company {
-  private Integer id;
+	private Integer id;
 
-  private String name;
+	private String name;
 
-  private String address;
+	private String address;
 
-  private String phone;
-
-  @Embedded
-  private ContactPerson contactPerson;
+	private String phone;
+	@Embedded
+	@AttributeOverrides({
+			@AttributeOverride(name = "firstName", column = @Column(name = "contact_first_name")),
+			@AttributeOverride(name = "lastName", column = @Column(name = "contact_last_name")),
+			@AttributeOverride(name = "phone", column = @Column(name = "contact_phone"))
+	})
+	private ContactPerson contactPerson;
 }
